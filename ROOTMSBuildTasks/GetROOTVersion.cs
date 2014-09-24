ad0b9dbc85a48caf6849d3874259df630025df5e
Gordon Watts <gordon@gordonandpaula.net>
2014-09-24 15:35:36 +0200

    msbuild to find installed root version

---
+++ ROOTMSBuildTasks/GetROOTVersion.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ROOTMSBuildTasks
+{
+    /// <summary>
+    /// MSBuild task that will return "none" or the version number "5.34.01" for whatever current
+    /// is the current version of ROOT.
+    /// 
+    /// Look for the ROOT version number in the following locations:
+    ///   1) ROOTSYS env var
+    ///   2) c:\root
+    ///   
+    /// To find the version number given one of the above directories is found:
+    /// 
+    ///   1) etc/gitinfo.txt, the first line, with the leading "v" stripped off.
+    ///   
+    /// If either of these things fails, then the string "notfound" is returned. 
+    /// </summary>
+    public class GetROOTVersion : Task
+    {
+        /// <summary>
+        /// Returns the current version of ROOT installed on the machine.
+        /// </summary>
+        [Output]
+        public string Version { get; private set; }
+
+        /// <summary>
+        /// Look for the ROOT install and the version number.
+        /// </summary>
+        /// <returns>Always return success</returns>
+        public override bool Execute()
+        {
+            Version = "notfound";
+
+            // Try the ROOTSYS number first.
+            var rootsys = Environment.GetEnvironmentVariable("ROOTSYS");
+            if (!directory_ok(rootsys))
+            {
+                rootsys = @"c:\root";
+                if (!directory_ok(rootsys))
+                    return true;
+            }
+
+            // Extract the version number.
+            var v = get_git_version(rootsys);
+            if (v != null)
+            {
+                Version = v;
+                return true;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Given rootsys, look for the version number in a git file.
+        /// </summary>
+        /// <param name="rootsys"></param>
+        /// <returns></returns>
+        private string get_git_version(string rootsys)
+        {
+            var gitv_file = Path.Combine(new string[] { rootsys, @"etc\gitinfo.txt" });
+            if (!File.Exists(gitv_file))
+                return null;
+
+            var vl = File.ReadAllLines(gitv_file);
+            if (vl.Length < 1)
+                return null;
+            if (!vl[0].StartsWith("v"))
+                return null;
+            return vl[0].Substring(1);
+        }
+
+        /// <summary>
+        /// Is this a directory we might use to look for ROOT?
+        /// </summary>
+        /// <param name="rootsys"></param>
+        /// <returns></returns>
+        private bool directory_ok(string rootsys)
+        {
+            return !string.IsNullOrWhiteSpace(rootsys) && Directory.Exists(rootsys);
+        }
+    }
+}
